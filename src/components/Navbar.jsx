4cc5ba39c75011735bb75395e176a71a7a275fd0
TusharKaw <108132018+TusharKaw@users.noreply.github.com>
2024-08-15 17:26:51 +0530

    svgs added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import {menu, close, logo} from '../assets';
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className='w-screeen h-[80px] z-10 bg-white fixed drop-shadow-lg'>
+      <div className='flex jusify-between items-center w-full h-full md:max-w-[1240px] m-auto'>
+        
+        <div className='flex items-center'>
+          <img src={logo} alt="logo" className='sm:ml-10 ss:ml-10 md:ml-3 opacity-[55%] w-full h-[25px]'/>
+        </div>
+
+        <div className='flex items-right'>
+          <img src={close} alt="close" className='sm:ml-10 ss:ml-10 md:ml-3 opacity-[55%] w-full h-[25px]'/>
+        </div>
+
+        <div className='flex items-right'>
+          <img src={menu} alt="menu" className='sm:ml-10 ss:ml-10 md:ml-3 opacity-[55%] w-full h-[25px]'/>
+        </div>
+
+      </div>
     </div>
   )
 }
